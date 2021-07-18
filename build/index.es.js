22d7343352a897c719ee3b423ccd20d17d6fbd58
AnirudhKumar10 <anirudh3104@gmail.com>
2021-07-19 03:16:56 +0530

    fix: add build to gitignore

--- build/index.es.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-var MediaUploader = function () {
-    return React.createElement("div", null, "Hello");
-};
-
-export { MediaUploader };
-//# sourceMappingURL=index.es.js.map
