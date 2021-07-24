6ee068520362031e2e85b67f32d61e852986b4c4
AnirudhKumar10 <anirudh3104@gmail.com>
2021-07-24 19:17:33 +0530

    docs: add html for cra example

---
+++ example/cra/src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+import { MediaUploader } from 'react-media-uploader'
+
+function App() {
+  return (
+    <div className="App">
+      <MediaUploader />
+    </div>
+  )
+}
+
+export default App
